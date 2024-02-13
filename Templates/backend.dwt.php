346237d31393bd320104b411d1988c3d15934cce
chicalo3 <55538163+chicalo3@users.noreply.github.com>
2024-02-13 11:52:35 +0100

    Add files via upload

---
+++ Templates/backend.dwt.php
@@ -0,0 +1,28 @@
+<?php require_once('../Connections/conexiontienda.php'); ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Documento sin título</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+<link href="../estilos-admin.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class="contenedor-admin">
+  <div class="cabecera-admin">
+   <div class="logo-admin"><img src="../fotos-productos/gestiondeuda.jpeg" width="275" height="137" /></div>
+    <div class="texto-administracion"><h2>ADMINISTRACION</h2></div>
+    
+  </div>
+  <div class="icono-home"><a href="../admin/index.php" title="Inicio"></a></div>
+  <!-- TemplateBeginEditable name="EditRegion3" -->
+  
+  <div class="contenido-administracion"></div>
+  <!-- TemplateEndEditable --></div>
+</body>
+</html>
